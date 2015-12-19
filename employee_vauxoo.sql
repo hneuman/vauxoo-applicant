fe50dbe8f9c609631b1d6a5262440795b4547383
hernanneuman15123123 <hernanneuman15@hotmail.com>
2015-12-19 15:26:47 -0500

    hernanneuman, segundo punto

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -10,7 +10,6 @@ CREATE DATABASE employee_employee WITH OWNER hache;
 
 
 
-
 CREATE TABLE employee (
 );
 
@@ -23,16 +22,31 @@ CREATE TABLE employee_hobby (
 
 
 
-ALTER TABLE  employee_department   ADD id serial   PRIMARY KEY    NOT NULL;
-ALTER TABLE  employee_department   ADD name CHAR(120) NOT NULL;
-ALTER TABLE employee_department   ADD  description text;
+ALTER TABLE  employee_hobby   ADD id serial   PRIMARY KEY    NOT NULL;
+ALTER TABLE  employee_hobby   ADD name CHAR(120) NOT NULL;
+ALTER TABLE employee_hobby   ADD  description text;
+
+
 
 ALTER TABLE employee   ADD id serial   PRIMARY KEY  NOT NULL;
 ALTER TABLE  employee   ADD first_name  CHAR(120) NOT NULL;
 ALTER TABLE  employee   ADD last_name  CHAR(120) NOT NULL;
-ALTER TABLE  employee   ADD department_id integer ;
-ALTER TABLE  employee   ADD FOREIGN KEY (department_id) REFERENCES employee_department(id);
 
+
+-- This is the junction table.
+CREATE TABLE employee_hobby_relacion (
+	id serial    NOT NULL,
+    employee_id integer REFERENCES employee (id),
+    employee_hobby_id integer REFERENCES employee_hobby (id),
+    PRIMARY KEY (employee_id, employee_hobby_id)
+)
+
+
+
+
+INSERT INTO employee_hobby (name,description) VALUES ('Leer','LEER de ');
+INSERT INTO employee_hobby (name,description) VALUES ('Programar',' PC de ');
+INSERT INTO employee_hobby (name,description) VALUES ('Correr',' EN la calle ');
 
 
 INSERT INTO employee_department (name,description) VALUES ('tecnologia','departamento de tecnologia');
@@ -46,3 +60,13 @@ INSERT INTO employee (first_name,last_name,department_id) VALUES ('HERNAN','N',1
 INSERT INTO employee (first_name,last_name,department_id) VALUES ('EDGAR','R',2);
 INSERT INTO employee (first_name,last_name,department_id) VALUES ('DEIVIS','L',2);
 INSERT INTO employee (first_name,last_name,department_id) VALUES ('MAURICIo','S',5);
+
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('1','1');
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('1','2');
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('2','2');
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('2','3');
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('3','1');
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('3','3');
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('4','2');
+INSERT INTO employee_hobby_relacion (employee_id,employee_hobby_id) VALUES ('4','1');
+
